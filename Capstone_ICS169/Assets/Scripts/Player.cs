e6360590892bf97af961323bd9e6fa7c36b54694
byeh300 <byeh300@gmail.com>
2020-10-28 15:09:57 -0700

    added health/bar, press v to selfdamage

--- Capstone_ICS169/Assets/Scripts/Player.cs
+++ Capstone_ICS169/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -10,7 +11,9 @@ public class Player : MonoBehaviour
 	private float _speed = 5f;
 	[SerializeField]
 	private float _gravity = 9.82f;
-
+	public int health = 100;
+	public float cooldown;
+	public Image healthbar;
 
 	void Start()
 	{
@@ -24,6 +27,30 @@ public class Player : MonoBehaviour
 	void Update()
 	{
 		CalculateMovement();
+		if (Input.GetKey(KeyCode.V) && cooldown == 0)
+		{
+			cooldown = 2;
+			health -= 15;
+			print("HP: " + (health + 15) + " -> " + health);
+			if (health < 0)
+			{
+				print("Game Over");
+				healthbar.enabled = false;
+			}
+			
+			else
+			{
+				healthbar.transform.localScale = new Vector3(health / 100f, 1, 1);
+				healthbar.transform.localPosition = new Vector2((1f - healthbar.transform.localScale.x) * -250f, healthbar.transform.localPosition.y);
+			}
+				
+
+		}
+
+		if (cooldown > 0)
+			cooldown -= Time.deltaTime;
+		if (cooldown <= 0)
+			cooldown = 0;
 	}
 	
 	void CalculateMovement()
@@ -37,4 +64,6 @@ public class Player : MonoBehaviour
 		velocity = transform.TransformDirection(velocity);
 		_controller.Move(velocity * Time.deltaTime * _speed);
 	}
+
+	
 }
