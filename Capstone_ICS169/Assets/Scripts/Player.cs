f56cac04d97e93efceedc5dccc5379c6ce579ce8
Aleroms <santiagomorales333@yahoo.com>
2020-10-27 21:32:27 -0700

    Added Probuilder Level and simple FPS script

---
+++ Capstone_ICS169/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private CharacterController _controller;
+
+	[SerializeField]
+	private float _speed = 5f;
+	[SerializeField]
+	private float _gravity = 9.82f;
+
+	void Start()
+	{
+		_controller = GetComponent<CharacterController>();
+
+		if (_controller == null) Debug.LogError("controller is null");
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+	{
+		CalculateMovement();
+
+		if(Input.GetKey(KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+
+	}
+	void CalculateMovement()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		Vector3 direction = new Vector3(x, 0, z);
+		Vector3 velocity = direction * _speed;
+		velocity.y -= _gravity;
+
+		velocity = transform.TransformDirection(velocity);
+		_controller.Move(velocity * Time.deltaTime * _speed);
+	}
+}
