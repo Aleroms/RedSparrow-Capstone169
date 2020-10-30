8562fd6c2e8e70e1c94eed4345d89f265d2df74a
Aleroms <santiagomorales333@yahoo.com>
2020-10-29 17:55:35 -0700

    moved some of brian's health code to healthUI.cs

--- Capstone_ICS169/Assets/Scripts/Player.cs
+++ Capstone_ICS169/Assets/Scripts/Player.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
@@ -11,9 +11,9 @@ public class Player : MonoBehaviour
 	private float _speed = 5f;
 	[SerializeField]
 	private float _gravity = 9.82f;
-	public int health = 100;
-	public float cooldown;
-	public Image healthbar;
+	[SerializeField]
+	private int health = 100;
+	
 
 	void Start()
 	{
@@ -27,30 +27,7 @@ public class Player : MonoBehaviour
 	void Update()
 	{
 		CalculateMovement();
-		if (Input.GetKey(KeyCode.V) && cooldown == 0)
-		{
-			cooldown = 2;
-			health -= 15;
-			print("HP: " + (health + 15) + " -> " + health);
-			if (health < 0)
-			{
-				print("Game Over");
-				healthbar.enabled = false;
-			}
-			
-			else
-			{
-				healthbar.transform.localScale = new Vector3(health / 100f, 1, 1);
-				healthbar.transform.localPosition = new Vector2((1f - healthbar.transform.localScale.x) * -250f, healthbar.transform.localPosition.y);
-			}
-				
-
-		}
-
-		if (cooldown > 0)
-			cooldown -= Time.deltaTime;
-		if (cooldown <= 0)
-			cooldown = 0;
+		
 	}
 	
 	void CalculateMovement()
@@ -63,6 +40,10 @@ public class Player : MonoBehaviour
 
 		velocity = transform.TransformDirection(velocity);
 		_controller.Move(velocity * Time.deltaTime * _speed);
+	}
+	public int GetCurrentHealth()
+	{
+		return health;
 	}
 
 	
