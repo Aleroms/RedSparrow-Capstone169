754bd5c2a438bac0203c0676b535226a96ffc831
Aleroms <santiagomorales333@yahoo.com>
2020-10-29 19:56:19 -0700

    player jumping works

--- Capstone_ICS169/Assets/Scripts/Player.cs
+++ Capstone_ICS169/Assets/Scripts/Player.cs
@@ -7,13 +7,24 @@ public class Player : MonoBehaviour
 {
 	private CharacterController _controller;
 
+	public bool _isgrounded;
+
 	[SerializeField]
 	private float _speed = 5f;
 	[SerializeField]
 	private float _gravity = 9.82f;
 	[SerializeField]
+	private float _jumpHeight = 15f;
+	private float _yVelocity;
+
+	[SerializeField]
 	private int health = 100;
+
+	public Transform groundCheck;
+	public float groundDistance = 0.4f;
+	public LayerMask groundMask;
 	
+	Vector3 velocity;
 
 	void Start()
 	{
@@ -32,14 +43,31 @@ public class Player : MonoBehaviour
 	
 	void CalculateMovement()
 	{
+		_isgrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+		if(_isgrounded && velocity.y < 0f)
+		{
+			velocity.y = -2f;
+		}
 		float x = Input.GetAxis("Horizontal");
 		float z = Input.GetAxis("Vertical");
-		Vector3 direction = new Vector3(x, 0, z);
-		Vector3 velocity = direction * _speed;
-		velocity.y -= _gravity;
+		Vector3 direction = x * transform.right + z * transform.forward;
 
-		velocity = transform.TransformDirection(velocity);
-		_controller.Move(velocity * Time.deltaTime * _speed);
+		_controller.Move(direction * Time.deltaTime * _speed);
+
+		if(Input.GetButtonDown("Jump") && _isgrounded)
+		{
+			velocity.y = Mathf.Sqrt(-2f * _jumpHeight * -1 *_gravity);
+		}
+
+		velocity.y -= _gravity * Time.deltaTime;
+
+		_controller.Move(velocity * Time.deltaTime);
+		
+	}
+	void OnTriggerEnter()
+	{
+		_isgrounded = true;//jumping
 	}
 	public int GetCurrentHealth()
 	{
