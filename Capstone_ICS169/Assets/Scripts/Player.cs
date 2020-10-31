11a204e6d34d45fe70d59c2d053c3c59d952fe8d
Aleroms <santiagomorales333@yahoo.com>
2020-10-30 22:50:23 -0700

    Implemented Sprint. Working on getting a cooldown to work

--- Capstone_ICS169/Assets/Scripts/Player.cs
+++ Capstone_ICS169/Assets/Scripts/Player.cs
@@ -11,7 +11,11 @@ public class Player : MonoBehaviour
 	private bool _isgrounded;
 	[SerializeField]
 	private bool _iscrouching;
+	[SerializeField]
+	private bool _issprinting;
 
+	[SerializeField]
+	private float _speedOriginal;
 	[SerializeField]
 	private float _speed = 5f;
 	[SerializeField]
@@ -22,6 +26,14 @@ public class Player : MonoBehaviour
 	private float _crouchHeight = 0.8f;
 	[SerializeField]
 	private float _groundDistance = 0.4f;
+	[SerializeField]
+	private float _sprintCooldown = 1f;
+	[SerializeField]
+	private float _stamina = 3f;
+	[SerializeField]
+	private float _maxStamina = 5f;
+	[SerializeField]
+	private float _sprintSpeed = 10f;
 
 	[SerializeField]
 	private int _health = 100;
@@ -38,6 +50,10 @@ public class Player : MonoBehaviour
 
 	void Start()
 	{
+		_issprinting = false;
+		_speedOriginal = _speed;
+		_stamina = _maxStamina;
+
 		_controller = GetComponent<CharacterController>();
 
 		if (_controller == null) Debug.LogError("controller is null");
@@ -50,6 +66,7 @@ public class Player : MonoBehaviour
 		Movement();
 		Jump();
 		Crouch();
+		Sprint();
 	}
 	
 	void Movement()//function works properly when proper layerMask is set to ground. 
@@ -88,12 +105,35 @@ public class Player : MonoBehaviour
 			_controller.height = 0.8f;
 		}
 		
-
+		//negates the player from standing up when under obstacle v v 
 		if(Input.GetKeyUp(KeyCode.LeftControl) && _iscrouching == false)
 		{
 			_controller.height = 1.8f;
 		}
 	
+	}
+	void Sprint()
+	{	//current bug. I can sprint then jump but sprinting immediately stops if jumping
+		// I wanted to stop player from jumping, then sprinting.
+		//don't want player to sprint if crouching
+
+		if (Input.GetKey(KeyCode.LeftShift) && _iscrouching == false && _isgrounded && _stamina > 0.2f)
+		{
+			_stamina -= Time.deltaTime;
+			_speed = _sprintSpeed;
+			_issprinting = true;
+		}
+		else
+		{
+			if (_stamina < 0f)
+				_stamina = 0f;
+
+			_issprinting = false;
+			_speed = _speedOriginal;
+
+			if (_maxStamina > _stamina)
+				_stamina += Time.deltaTime;
+		}
 	}
 	
 	public int GetCurrentHealth()
