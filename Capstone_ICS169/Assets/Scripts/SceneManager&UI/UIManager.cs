a6cc297512c664cc1022ebdf196b8fdfaaacd4c9
Aleroms <santiagomorales333@yahoo.com>
2020-11-02 15:45:03 -0800

    Added signs to level and created Main Menu & Credits

---
+++ Capstone_ICS169/Assets/Scripts/SceneManager&UI/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	[SerializeField]
+	private Button _playButton;
+	[SerializeField]
+	private Button _quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void Quit()
+	{
+		print("Closing Scene");
+		Application.Quit();
+	}
+}
---
+++ Capstone_ICS169/Assets/Scripts/SceneManager&UI/firstDeliverable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//sends player to credits menu
+public class firstDeliverable : MonoBehaviour
+{
+    //basic way to end the game for our first
+	//deliverable.
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			SceneManager.LoadScene("Credits");
+		}
+	}
+}
