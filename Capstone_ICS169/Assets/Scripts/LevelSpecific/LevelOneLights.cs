e12a178ceb9ba34e9c668a16399344541034ae81
FamousHero <33110962+FamousHero@users.noreply.github.com>
2020-11-09 14:39:15 -0800

    Added lights to dome bridge and script

---
+++ Capstone_ICS169/Assets/Scripts/LevelSpecific/LevelOneLights.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneLights : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject DomeBridge;
+    [SerializeField]
+    private GameObject DomePlatform;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Start()
+    {
+        if(DomePlatform.activeSelf)
+        {
+            DomePlatform.SetActive(false);
+        }
+        if(!DomeBridge.activeSelf)
+        {
+            DomeBridge.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            DomeBridge.SetActive(false);
+            DomePlatform.SetActive(true);
+        }
+    }
+}
