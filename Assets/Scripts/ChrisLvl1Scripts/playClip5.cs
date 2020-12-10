59149a88105733e288c7db6961d28b5649914acf
Christopher Dang <christhd@uci.edu>
2020-12-10 09:32:29 -0800

    Made a different version of level 1 teaches the very basics of looking and moving introduces the checkpoint system

---
+++ Assets/Scripts/ChrisLvl1Scripts/playClip5.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playClip5 : MonoBehaviour
+{
+    public GodHand gh;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            gh.playClip(5);
+        }
+    }
+}
